cc17a7ad032b5338fff944b60e24b4ae4758ac1d
Ashutosh Champatiray <asshutoshjena5@gmail.com>
2026-04-06 14:37:26 +0530

    Fix import statement for EventManagementSystem.Entity

--- EventManagementSystem/DAL/EventDAL.cs
+++ EventManagementSystem/DAL/EventDAL.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using EventManagementSystem.Entity;
+using EventManagementSystem.Entity
 
 namespace EventManagementSystem.DAL
 {
